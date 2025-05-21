fdadd284bf1b6915b8702af00b6a5b73cb8e6222
rmfatemi <rmfatemi@gmail.com>
2025-05-21 00:17:26 -0400

    new config and implementation for ti

---
+++ nutalert/parser.py
@@ -0,0 +1,22 @@
+import re
+from nutalert.utils import setup_logger
+
+
+logger = setup_logger("parser")
+
+
+def parse_nut_data(raw_data):
+    pattern = re.compile(r'^VAR ups\s+([^ ]+)\s+"([^"]+)"$')
+    nut_values = {}
+    for line in raw_data.splitlines():
+        m = pattern.match(line.strip())
+        if m:
+            key, value = m.groups()
+            try:
+                nut_values[key] = int(value)
+            except ValueError:
+                try:
+                    nut_values[key] = float(value)
+                except ValueError:
+                    nut_values[key] = value.strip()
+    return nut_values
